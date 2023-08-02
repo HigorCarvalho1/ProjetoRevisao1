4dacc8460733355dee0200b2cde2cf349de40c5b
HigorCarvalho1 <136502008+HigorCarvalho1@users.noreply.github.com>
2023-08-02 19:41:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let history = prompt(`Bem Vindos! 
+Este é o Primeiro Projeto da Revisão dos Stages 1 ao 4
+1.Se quiser saber mais sobre minha Historia 
+2.Ir para o projeto`)
+if(history == 1) {
+    alert(`Olá Meu nome é Higor Carvalho estou começando na area de Programador agora, no momento eu sou Policial Militar do Estado de Sp mas estou tentando conciliar o serviço com os Estudos e com a ajuda da RockSeat estou conseguindo aprender.
+    Obrigado por ler uma breve introdução da minha historia.
+    Um Abraço S2 `)
+}else{
+    alert("Vamos lá!")
+}
